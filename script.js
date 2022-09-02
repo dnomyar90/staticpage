2c358a3d0e743d6d952fc50375d3060d517e6c7c
Raymond <67101351+dnomyar90@users.noreply.github.com>
2022-09-02 14:42:30 +0800

    Update

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ if (url.searchParams.get('by') != null) {
 }
 
 document.querySelector(".tombol").addEventListener('click', function () {
-  Swal.fire("Hallo Sayangku", "Aku ada pertanyaan nih buat kamu?", "question").then(function () {
-    Swal.fire("Jawab yang jujur ya!").then(function () {
-      Swal.fire("Awas aja kalo boong!!", "", "error").then(function () {
+  Swal.fire("Hallo Sayangku", "Aku mau minta maaf yaaaaa", "question").then(function () {
+    Swal.fire("Aku tau aku yang salah!").then(function () {
+      Swal.fire("Maafin aku yaa?", "", "success").then(function () {
 
         const {
           value: name
